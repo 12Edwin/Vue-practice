165503566dbb27deba351d0cd19ae287b78400d3
Edwin <20213tn046@utez.edu.mx>
2024-01-12 22:42:41 -0600

    create app

---
+++ Cliente/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <b-button pill v-b-toggle.sidebar-variant></b-button>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="50" height="50" />
+      
+    </header>
+
+    <div class="main">
+      <b-sidebar id="sidebar-variant" class="sidebar" title="Sidebar" bg-variant="dark" text-variant="light" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+      <div class="content">
+      </div> 
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  header{
+    display: flex;
+    width: 100%;
+    align-items: center;
+    height: 8vh;
+    background-color: antiquewhite;
+    padding-left: 50px;
+  }
+  .main{
+    height: 92vh;
+    background-color: rgb(107, 138, 138);
+    display: flex;
+  }
+
+  .sidebar{
+    width: 18vw;
+    height: 92vh;
+    position: relative;
+  }
+
+
+</style>
+
+<style>
+  .b-sidebar{
+    position: relative;
+  }
+</style>
---
+++ Cliente/src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+new Vue({
+  render: (h) => h(App)
+}).$mount('#app')
